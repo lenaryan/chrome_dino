d6779822a069ac8514a82f72deb6c2037809141a
Lena Ryan <alissadyiphil@gmail.com>
2023-08-19 22:05:55 +0600

    added ground

---
+++ src/scenes/PlayScene.ts
@@ -0,0 +1,14 @@
+import Phaser from "phaser";
+
+class PlayScene extends Phaser.Scene {
+    constructor() {
+        super('PlayScene');
+    }
+
+    create() {
+        this.add.tileSprite(0, 340, 88, 26, 'ground')
+            .setOrigin(0,1);
+    }
+}
+
+export default PlayScene;

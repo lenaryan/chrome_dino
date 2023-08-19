851ea610ebb515cd5ae909c6f1506de369d8d070
Lena Ryan <alissadyiphil@gmail.com>
2023-08-19 22:17:13 +0600

    added dino

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,13 +1,27 @@
 import Phaser from "phaser";
 
 class PlayScene extends Phaser.Scene {
+    get gameHeight() {
+        return this.game.config.height as number;
+    }
+
     constructor() {
         super('PlayScene');
     }
 
     create() {
-        this.add.tileSprite(0, 340, 88, 26, 'ground')
-            .setOrigin(0,1);
+        this.createEnv();
+        this.createPlayer();
+    }
+
+    createEnv() {
+        this.add
+            .tileSprite(0, this.gameHeight, 88, 26, 'ground')
+            .setOrigin(0, 1);
+    }
+
+    createPlayer() {
+        this.physics.add.sprite(0, this.gameHeight, 'dino').setOrigin(0, 1);
     }
 }
 

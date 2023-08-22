29081f9de304ea1c1dd0729a451d72d1d643438e
Lena Ryan <alissadyiphil@gmail.com>
2023-08-22 13:16:49 +0600

    added gravity to dino

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,6 +1,9 @@
 import Phaser from "phaser";
+import { SpriteWithDynamicBody } from "../types";
 
 class PlayScene extends Phaser.Scene {
+    player: SpriteWithDynamicBody;
+
     get gameHeight() {
         return this.game.config.height as number;
     }
@@ -12,6 +15,7 @@ class PlayScene extends Phaser.Scene {
     create() {
         this.createEnv();
         this.createPlayer();
+        this.registerPlayerControl();
     }
 
     createEnv() {
@@ -21,7 +25,19 @@ class PlayScene extends Phaser.Scene {
     }
 
     createPlayer() {
-        this.physics.add.sprite(0, this.gameHeight, 'dino').setOrigin(0, 1);
+        this.player = this.physics.add.sprite(0, this.gameHeight, 'dino').setOrigin(0, 1);
+        this.player
+            .setGravityY(5000)
+            .setCollideWorldBounds(true)
+            .setBodySize(44, 92);
+    }
+
+    registerPlayerControl() {
+        const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        spaceBar.on('down', () => {
+            this.player.setVelocityY(-1600);
+            
+        })
     }
 }
 

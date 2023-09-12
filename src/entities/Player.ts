3ef9d94c13640ba561885cab97df530faff3d31c
Lena Ryan <alissadyiphil@gmail.com>
2023-09-12 13:03:11 +0600

    created player class

---
+++ src/entities/Player.ts
@@ -0,0 +1,17 @@
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene: Phaser.Scene, x: number, y: number, key: string) {
+        super(scene, x, y, key);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.init();
+    }
+
+    init() {
+        this
+            .setOrigin(0, 1)
+            .setGravityY(5000)
+            .setCollideWorldBounds(true)
+            .setBodySize(44, 92);
+    }
+}
--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,8 +1,9 @@
 import Phaser from "phaser";
 import { SpriteWithDynamicBody } from "../types";
+import { Player } from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
-    player: SpriteWithDynamicBody;
+    player: Player;
     startTrigger: SpriteWithDynamicBody;
 
     get gameHeight() {
@@ -34,11 +35,7 @@ class PlayScene extends Phaser.Scene {
     }
 
     createPlayer() {
-        this.player = this.physics.add.sprite(0, this.gameHeight, 'dino').setOrigin(0, 1);
-        this.player
-            .setGravityY(5000)
-            .setCollideWorldBounds(true)
-            .setBodySize(44, 92);
+        this.player = new Player(this, 0, this.gameHeight, 'dino');
     }
 
     registerPlayerControl() {

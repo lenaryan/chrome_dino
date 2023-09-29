b56fc092c3b69f7e21bdd29575e600f30dbf500b
Lena Ryan <alissadyiphil@gmail.com>
2023-09-29 13:34:21 +0600

    roll the ground

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -4,10 +4,15 @@ import { Player } from "../entities/Player";
 
 class PlayScene extends Phaser.Scene {
     player: Player;
+    ground: Phaser.GameObjects.TileSprite;
     startTrigger: SpriteWithDynamicBody;
 
     get gameHeight() {
         return this.game.config.height as number;
+    }
+
+    get gameWidth() {
+        return this.game.config.width as number;
     }
 
     constructor() {
@@ -15,26 +20,42 @@ class PlayScene extends Phaser.Scene {
     }
 
     create() {
-        this.createEnv();
+        this.createEnvironment();
         this.createPlayer();
         this.startTrigger = this.physics.add.sprite(0, 10, null)
             .setAlpha(0)
             .setOrigin(0, 1);
 
         this.physics.add.overlap(this.startTrigger, this.player, () => {
-            console.log('collision');
-            
+            if (this.startTrigger.y === 10) {
+                this.startTrigger.body.reset(0, this.gameHeight);
+                return;
+            }
+
+            this.startTrigger.body.reset(9999, 9999);
+            this.time.addEvent({
+                delay: 1000 / 60,
+                loop: true,
+                callback: () => {
+                    if (this.ground.width < this.gameWidth) {
+                        this.ground.width += 35;
+                    }
+                }
+            })
         })
     }
 
-    createEnv() {
-        this.add
+    createEnvironment() {
+        this.ground = this.add
             .tileSprite(0, this.gameHeight, 88, 26, 'ground')
             .setOrigin(0, 1);
     }
 
     createPlayer() {
         this.player = new Player(this, 0, this.gameHeight, 'dino');
+    }
+
+    update(time: number, delta: number): void {
     }
 }
 
